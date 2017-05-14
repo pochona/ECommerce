dbd69ff6d2c18250eb1e9790919d906894c64c3b
amelienM <amelien.mersch@gmail.com>
2017-05-15 00:32:24 +0200

    Pas le bon articleFacadeRemote sur l'ancien commit

--- ECommerce-shared/src/services/ArticleFacadeRemote.java
+++ ECommerce-shared/src/services/ArticleFacadeRemote.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package controllers;
+package services;
 
 import java.util.List;
 import javax.ejb.Remote;
@@ -17,12 +17,12 @@ import javax.ejb.Remote;
 public interface ArticleFacadeRemote {
     
     // Création d'un produit (pour Back Office)
-	String creer(Integer id, String lib, String description, double prixHt, float tauxTva, int stock);
-	// Liste les produits sous la forme de chaînes
-	List<String> lister();
-	// Renvoie les infos d'un produit
-	public String infos(long id);
-	// Renvoie le prix d'un produit
-	public double prix(long id);
+    String creer(Integer id, String lib, String description, double prixHt, float tauxTva, int stock);
+    // Liste les produits sous la forme de chaînes
+    List<String> lister();
+    // Renvoie les infos d'un produit
+    public String infos(long id);
+    // Renvoie le prix d'un produit
+    public double prix(long id);
     
 }
